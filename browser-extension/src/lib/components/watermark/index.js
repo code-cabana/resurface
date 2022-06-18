92d284826f674fa73da01fb754984cde45d78585
Loki <sneyed@gmail.com>
2022-06-18 15:27:45 +1000

    fix: [xtn] #11 disable watermark links and buttons when not visible

--- browser-extension/src/lib/components/watermark/index.js
+++ browser-extension/src/lib/components/watermark/index.js
@@ -5,19 +5,24 @@ import { ButtonLink, LinkButton } from "shared/ui";
 import { useSwellProduct } from "shared/hooks";
 import styles from "./styles.module.css";
 
-export default function Watermark({ opacity }) {
+export default function Watermark({ visible }) {
   const { loading, product } = useSwellProduct();
   const { price } = product || {};
+  const style = visible
+    ? { opacity: 1 }
+    : { opacity: 0, "pointer-events": "none" };
 
   return (
-    <div className={styles.container} style={{ opacity }}>
+    <div disabled={!visible} className={styles.container} style={style}>
       <span className={styles.text}>
         Please support Code Cabana to remove this watermark
       </span>
       <ButtonLink
         href={accountPage}
         target="_blank"
+        disabled={!visible}
         className={styles.buyButton}
+        style={style}
       >
         {loading ? "Buy Resurface" : `Buy Resurface now for $${price} USD`}
       </ButtonLink>
@@ -25,7 +30,9 @@ export default function Watermark({ opacity }) {
         Already subscribed?{" "}
         <LinkButton
           onClick={() => goToPage("assets/login.html")}
+          disabled={!visible}
           className={styles.loginButton}
+          style={style}
         >
           Login
         </LinkButton>
