17c53745e672dbd7ba78e25c6b992787811cafd2
Loki <lokimckay@gmail.com>
2022-12-27 12:15:16 +1000

    [xtn] add replit to devUrls

--- browser-extension/src/config/ci.js
+++ browser-extension/src/config/ci.js
@@ -3,6 +3,7 @@ module.exports = {
     "https://en.wikipedia.org/wiki/Web_page",
     "https://app.showit.co/demo?dk=dk:anj5daz_s96p14wtg5wxyg",
     "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_textarea",
+    "https://resurface.lokimckay.repl.co/",
     "https://login.squarespace.com/",
   ],
 };
